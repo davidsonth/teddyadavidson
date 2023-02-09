cfab225747469af3cb4dce4ef53698595b440b2e
davidsonth <davidson.th@northeastern.edu>
2023-02-08 21:11:59 -0500

    added navbar

---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const About = () => {
+    return (
+    <div>
+        <h3>This website was created in early 2023</h3>
+    </div>
+    );
+}
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const Home = () => {
+    return (
+    <div>
+        <h3>Teddy Davidson knows Typescript</h3>
+    </div>
+    );
+}
+export default Home;
